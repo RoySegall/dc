3f5474fa9fc3aed92696133c6f43cafc47199e60
RoySegall <roy.segall@gizra.com>
2013-10-12 13:09:26 +0300

    Remove documentation - this is not part of this module.

--- dc_apps/dc_apps.api.php
+++ dc_apps/dc_apps.api.php
@@ -18,6 +18,9 @@
  * Using the generateLink methods will be similar like this:
  *  $app = dc_apps_load(1);
  *  $link = $app->generateLink('new_methods');
+ *
+ * @pram $actions
+ *  List of actions that which the method can use.
  */
 function hook_dc_apps_generate_links_action_alter(&$actions) {
   $actions = array(
@@ -26,4 +29,19 @@ function hook_dc_apps_generate_links_action_alter(&$actions) {
       'href' => 'admin/apps/' . $this->id . '/new_action',
     ),
   );
+}
+
+/**
+ * Implements hook_dc_apps_app_options_alter().
+ *
+ * On the admin page each app has options that the user can apply on app. This
+ * hook allow you to add more option for each application.
+ *
+ * @param $items
+ *  The links to display for the user.
+ * @param DrupalConnectApps $app
+ *  The app object.
+ */
+function hook_dc_apps_app_options_alter(&$items, DrupalConnectApps $app) {
+  $items[] = $app->generateLink('new_action');
 }
