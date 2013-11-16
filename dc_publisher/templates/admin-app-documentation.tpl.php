bd6f380cef27d7ff1dca7c1e61be79261f4c4faa
RoySegall <roy.segall@gizra.com>
2013-11-17 00:32:45 +0200

    Improving the UX of the documentation page,=.

--- dc_publisher/templates/admin-app-documentation.tpl.php
+++ dc_publisher/templates/admin-app-documentation.tpl.php
@@ -1,18 +1,19 @@
-<div id="<?php print $tab_id; ?>" class="dc_publisher_documentation">
-  <ul>
-    <li><a href="#url">URL</a></li>
-    <li><a href="#code">Code</a></li>
-  </ul>
-  <p><?php print $title; ?></p>
-  <p><?php print $description; ?></p>
+<div class="dc_publisher_documentation">
+  <p class="title"><?php print $title; ?></p>
+  <p class="description"><?php print $description; ?></p>
+  <p class="url><?php print $url; ?></p>
 
-  <div id="url">
-    <p><?php print $raw_url; ?></p>
+  <div id="<?php print $tab_id; ?>" class="dc_publisher_tabs">
+    <ul class="list">
+      <li><a href="#url" class="url"><?php print $t['url']; ?></a></li>
+      <li><a href="#code" class="code"><?php print $t['code']; ?></a></li>
+    </ul>
+    <div id="url">
+      <p><?php print $raw_url; ?></p>
+    </div>
+
+    <div id="code">
+      <p><?php print $code; ?></p>
+    </div>
   </div>
-
-  <div id="code">
-    <p><?php print $code; ?></p>
-  </div>
-
-  <p><?php print $url; ?></p>
 </div>
